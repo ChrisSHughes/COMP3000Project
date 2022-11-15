255b3009f0d804cdc977ded5cc567b4fa91648fc
ChrisSHughes <72555296+ChrisSHughes@users.noreply.github.com>
2022-11-15 14:53:51 +0000

    interactions working
    i have created a gun that allows me to shoot bullets, as well as playing with filters to stop the user controlling things they shouldn't

--- VRCommander/Assets/Scripts/Shoot.cs
+++ VRCommander/Assets/Scripts/Shoot.cs
@@ -9,10 +9,25 @@ public class Shoot : MonoBehaviour
     public GameObject bulletObj;
     public Transform frontOfGun;
 
+    public AudioSource gunCock;
+    public AudioClip gunShot;
+    public AudioClip pickup;
+
+    public void Start()
+    {
+        gunCock = GetComponent<AudioSource>();
+    }
+
     public void Fire()
     {
+        gunCock.PlayOneShot(gunShot);
         GameObject pistolBullet = Instantiate(bulletObj, frontOfGun.position, frontOfGun.rotation);
         pistolBullet.GetComponent<Rigidbody>().velocity = velocity * frontOfGun.forward;
         Destroy(pistolBullet, 5f);
+    }
+
+    public void PickupSound()
+    {
+        gunCock.PlayOneShot(pickup);
     }
 }
