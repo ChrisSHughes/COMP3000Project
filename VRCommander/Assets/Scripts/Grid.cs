22aeb3efee07042da452acaa50d0f5186ea89da4
ChrisSHughes <72555296+ChrisSHughes@users.noreply.github.com>
2022-12-16 15:27:27 +0000

    Started writing dictionary stuff
    Created dictionary to hold values for coords and boolean. Also created a tuple..

--- VRCommander/Assets/Scripts/Grid.cs
+++ VRCommander/Assets/Scripts/Grid.cs
@@ -6,11 +6,20 @@ public class Grid : MonoBehaviour
 {
     public float size = 2f;
     public GameObject node;
-    public List<GameObject> coOrds = new List<GameObject>();
+    //public List<GameObject> coOrds = new List<GameObject>();
+
+    public Dictionary<Vector2, bool> dictCoords = new Dictionary<Vector2, bool>();
+
+    public (int, int, bool) TupleCoords = (0, 0, false);
+    
 
     public void Start()
     {
         DrawGridNodes();
+        foreach(KeyValuePair<Vector2, bool> pair in dictCoords)
+        {
+            Debug.Log("coord: " + pair.Key + " taken? = " + pair.Value);
+        }
     }
 
     public Vector3 GetNearestPointOnGrid(Vector3 position)
@@ -32,28 +41,31 @@ public class Grid : MonoBehaviour
 
         for (float x = 0; x < 200; x += size)
         {
-            Debug.Log("x = " + x);
+            //Debug.Log("x = " + x);
             for (float z = 0; z < 200; z += size)
             {
-                Debug.Log("z = " + z);
-                node = Instantiate(node,new Vector3(x + 1, 0f, z + 1), Quaternion.identity);
-                node.name = "x: " + x + ", z: " + z;
-                coOrds.Add(node);
-                node.transform.SetParent(transform);
+                //Debug.Log("z = " + z);
+                //node = Instantiate(node,new Vector3(x + 1, 0f, z + 1), Quaternion.identity);
+                //node.name = "x: " + x + ", z: " + z;
+                //coOrds.Add(node);
+                //node.transform.SetParent(transform);
+
+                Vector2 coord = new Vector2(x, z);
+                dictCoords.Add(coord, false);
             }
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.magenta;
-        for(float x = 0; x < 200; x += size)
-        {
-            for(float z = 0; z < 200; z += size)
-            {
-                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
-                Gizmos.DrawSphere(point, 0.5f);
-            }
-        }
+        //Gizmos.color = Color.magenta;
+        //for(float x = 0; x < 200; x += size)
+        //{
+        //    for(float z = 0; z < 200; z += size)
+        //    {
+        //        var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+        //        Gizmos.DrawSphere(point, 0.5f);
+        //    }
+        //}
     }
 }
